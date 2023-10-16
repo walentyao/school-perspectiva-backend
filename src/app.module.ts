d4a230b3b123e8d05edc72d4d6a035758dad53dc
walentyao <prikolnyyyvalk@yandex.ru>
2023-10-16 19:07:00 +0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './configs/mongo.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,24 @@
+import { ConfigService } from '@nestjs/config';
+import { MongooseModuleOptions } from '@nestjs/mongoose';
+export const getMongoConfig = async (
+  configService: ConfigService,
+): Promise<MongooseModuleOptions> => {
+  return {
+    uri: getMongoUriString(configService),
+  };
+};
+
+const getMongoUriString = (configService: ConfigService): string => {
+  return (
+    'mongodb://' +
+    configService.get('MONGO_LOGIN') +
+    ':' +
+    configService.get('MONGO_PASSWORD') +
+    '@' +
+    configService.get('MONGO_HOST') +
+    ':' +
+    configService.get('MONGO_PORT') +
+    '/' +
+    configService.get('MONGO_AUTHDATABASE')
+  );
+};
